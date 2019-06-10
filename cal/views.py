5375ac98a78cb01178fe13cee5809efbfe98cdd3
Derek <yangd01234567@gmail.com>
2019-06-10 15:46:24 -0700

    started initial django database

--- cal/views.py
+++ cal/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
+from .models import Time
 
-
-c_save_test = [
-    {
-        'username': 'test@gmail.com',
-        'save_name': 'test save 1',
-        'save_color': 'test save color code',
-        'start_time': 'test start time',
-        'end_time': 'test end time'
-    },
-    {
-         'username': 'test@gmail.com',
-        'save_name': 'test save 2',
-        'save_color': 'test save 2 color code',
-        'start_time': 'test start 2 time',
-        'end_time': 'test end 2 time'       
-    }
-]
 # homepage
 def home(request):
     context = {
-        'c_save':c_save_test
+        'c_save': Time.objects.all()
     }
     return render(request, 'cal/home.html', context)
 
