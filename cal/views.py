9c8529a5f5082d8b57cd545be786bf687db423a7
Derek <yangd01234567@gmail.com>
2019-06-14 15:46:52 -0700

    added CRUD functionality to times

--- cal/views.py
+++ cal/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.decorators import login_required
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Time
 
 # homepage
@@ -20,6 +21,46 @@ class TimeListView(ListView):
 class TimeDetailView(DetailView):
     model = Time
 
+class TimeCreateView(LoginRequiredMixin, CreateView):
+    model = Time
+    fields = ['title','color','start_time','end_time']
+
+    # set form author to current logged in user
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+class TimeUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Time
+    fields = ['title','color','start_time','end_time']
+
+    # set form author to current logged in user
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+    # validate if time belonds to user
+    def test_func(self):
+        model = Time
+
+        time = self.get_object()
+        if self.request.user == time.owner:
+            return True
+        else:
+            return False
+
+
+class TimeDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Time
+    success_url = '/'
+
+    # validate if time belonds to user
+    def test_func(self):
+        time = self.get_object()
+        if self.request.user == time.owner:
+            return True
+        else:
+            return False
 # about
 def about(request):
     return render(request, 'cal/about.html', {'title': 'About'})
