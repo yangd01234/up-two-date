573b685a4caeb782eb66f18a6437c9eb0136a7e4
Derek <yangd01234567@gmail.com>
2019-06-15 12:04:03 -0700

    completed password reset functionality

--- uptwodate/settings.py
+++ uptwodate/settings.py
@@ -125,5 +125,11 @@ STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'calendar-home'
-
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'noreplyuptwodate@gmail.com'
+EMAIL_HOST_PASSWORD = 'ggddxlyhzpaifmoy'
--- uptwodate/urls.py
+++ uptwodate/urls.py
@@ -21,6 +21,18 @@ urlpatterns = [
     path('password-reset/', 
         auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), 
         name='password_reset'),
+    # reset password reqest complete
+    path('password-reset/done/', 
+        auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), 
+        name='password_reset_done'),
+    # reset password reqest confirm
+    path('password-reset-confirm/<uidb64>/<token>/', 
+        auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), 
+        name='password_reset_confirm'),
+    # final step reset password
+    path('password-reset-complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), 
+        name='password_reset_complete'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
